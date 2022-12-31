c3caf2c4d27f489c609014a0fadd960eb8945bfd
Haven Tillman <htillma1@asu.edu>
2022-12-31 20:31:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("http").createServer((_, res) => res.end("Berjalan coy")).listen(8080)
 
-const sessionName = 'yusril'
-const donet = 'https://saweria.co/sansekai'
-const owner = ['6287878817169']
+const sessionName = 'session'
+const donet = 'supuna.ml'
+const owner = ['94704029017']
 const { default: sansekaiConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, getContentType } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
